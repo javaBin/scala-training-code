3a18d06b4bc82cae1263b63c86bea573f7814b02
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-08-03 19:30:58 +0200

    Start with some simple examples.

---
+++ intro/src/main/scala/scalaexamples/intro/HelloWorld.scala
@@ -0,0 +1,16 @@
+package scalaexamples.intro
+
+// Print out HelloWorld
+object HelloWorld1 extends Application {
+  	
+  println("Hello world1")
+	
+}
+
+object HelloWorld2 {
+  
+  def main(args: Array[String]) {
+    println("Hello world2")
+  }
+	
+}
