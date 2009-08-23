fc29523fbf9eec1716dde90c7ffe3bed299bd3ef
Fredrik Vraalsen <fredrik@vraalsen.no>
2009-08-23 17:11:15 +0200

    Add tail-recursive partition impl

--- higher-order-functions/src/main/scala/scalaexamples/higherorderfunctions/PersonPartitioner.scala
+++ higher-order-functions/src/main/scala/scalaexamples/higherorderfunctions/PersonPartitioner.scala
@@ -10,4 +10,17 @@ class PersonPartitioner {
       case Nil => (Nil, Nil)
     }
 
+  def tailPartition[A](l: List[A], f: A => Boolean): (List[A], List[A]) = {
+    def helper(l: List[A], f: A => Boolean, ftrue: List[A], ffalse: List[A]): (List[A], List[A]) = {
+      if (l.isEmpty) {
+        return (ftrue.reverse, ffalse.reverse)
+      }
+      val h = l.head
+      val (ftrue2, ffalse2) = if (f(h)) (h :: ftrue, ffalse) else (ftrue, h :: ffalse)
+      helper(l.tail, f, ftrue2, ffalse2)
+    }
+
+    helper(l, f, Nil, Nil)
+  }
+
 }
