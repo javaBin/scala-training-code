023ff79408c3c5b98c23da14c1a8221da53b0482
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-08-25 17:28:26 +0200

    Pattern matching on Strings with regexes.

---
+++ pattern-matching/src/test/scala/scalaexamples/RegexMatchingTest.scala
@@ -0,0 +1,54 @@
+package scalaexamples
+
+import junit.framework.Assert._
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(classOf[JUnit4])
+class RegexMatchingTest {
+  
+  @Test def matchEntireString {
+    val string = "This is the string to find a match for."
+    val matchRegex = "(.+)".r
+    
+    val mathedElement = string match {
+      case matchRegex(firstElement) => firstElement
+      //case s => s
+      case _ => "Failed"
+    }
+    
+    assertEquals(string, mathedElement)
+  }
+  
+  
+  @Test def matchFirstPartOfString {
+    val start = "This is"
+    val string = start + " the string to find a match for."
+    // val matchRegex = """(.{7}).*""".r
+    val matchRegex = """^(\w+\s\w+).*""".r
+    
+    val mathedElement = string match {
+      case matchRegex(firstElement) => firstElement
+      case _ => "Failed"
+    }
+    
+    assertEquals(start, mathedElement)
+  }
+  
+  @Test def matchFirstAndLastWordOfString {
+    val start = "This"
+    val end = "for"
+    val string = start + " is the string to find a match " + end
+    val matchRegex = """^(\w+).*?(\w+)$""".r
+    
+    val (first, last) = string match {
+      case matchRegex(firstElement, lastElement) => (firstElement, lastElement)
+      case _ => "Failed"
+    }
+    
+    assertEquals(start, first)
+    assertEquals(end, last)
+  }
+
+}
