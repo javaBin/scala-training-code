34c6b9f73a8b98d058b2481542ce3d090af6e0e5
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-09-24 07:50:02 +0200

    Pattern matching on tuples.

---
+++ pattern-matching/src/test/scala/scalaexamples/patternmatching/TupleTestMatchTest.scala
@@ -0,0 +1,42 @@
+package scalaexamples.patternmatching
+
+import junit.framework.Assert._
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(classOf[JUnit4])
+class TupleTestMatchTest {
+  
+  @Test def matchOnTupes {
+    val simpleTuple: Any = (1, "string")
+
+    simpleTuple match {
+      // Comment out next line
+      case (one, two) => assertEquals(simpleTuple, (one, two))
+      case _ => error("Couldnt match it")
+    }
+  }
+  
+  @Test def matchOnTupesWithGuard {
+    val simpleTuple: Any = (1, "string")
+
+    simpleTuple match {
+      // Comment out next line
+      case (one, _) if one == 1 => assertEquals(1, one)
+      case (one, _) => assertEquals(2, one)
+      case _ => error("Couldnt match it")
+    }
+  }
+
+  @Test def matchOnLargerTuple{
+    val simpleTuple: Any = (1, "string", false)
+    
+    simpleTuple match {
+      // Comment out next line
+      case (one, two, three: Boolean) => assertFalse(three)
+      case (one, two) => error("this is not a Tuple2")
+      case _ => error("Couldnt match it")
+    }
+  }
+}
