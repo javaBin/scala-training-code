2213e9aeb5366f8ead4448d44f0ee4c2725c5eaa
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-09-29 15:46:27 +0200

    The first test class that the users should play with.

---
+++ common/src/test/scala/scalaexamples/MyFirstTest.scala
@@ -0,0 +1,11 @@
+package scalaexamples
+
+import junit.framework.Assert._
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(classOf[JUnit4])
+class MyFirstTest extends EmptyTest{
+
+}
