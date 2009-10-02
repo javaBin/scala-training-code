81549804e64016e25f06db8387b1fcb97914c6a6
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-10-02 17:02:13 +0200

    Moved train into separate source file.

---
+++ oo-traits/src/main/scala/scalaexamples/oo_traits/Train.scala
@@ -0,0 +1,15 @@
+package scalaexamples.oo_traits
+
+
+class Train(val passengers: List[String], val conductor: Option[String]) {
+  
+  def this(passengers: List[String]) = this(passengers, None)
+}
+
+object Train {
+  
+  def apply(passengerName: Array[String]) = new Train(passengerName.toList)
+  def apply(passengerName: String*) = new Train(passengerName.toList)
+  def apply(conductor: Option[String], passengerName: String*) = new Train(passengerName.toList, conductor)
+  
+}
--- oo-traits/src/test/scala/scalaexamples/oo_traits/CompanionObjectTest.scala
+++ oo-traits/src/test/scala/scalaexamples/oo_traits/CompanionObjectTest.scala
@@ -33,16 +33,3 @@ class CompanionObjectTest {
   }
   
 }
-
-class Train(val passengers: List[String], val conductor: Option[String]) {
-  
-  def this(passengers: List[String]) = this(passengers, None)
-}
-
-object Train {
-  
-  def apply(passengerName: Array[String]) = new Train(passengerName.toList)
-  def apply(passengerName: String*) = new Train(passengerName.toList)
-  def apply(conductor: Option[String], passengerName: String*) = new Train(passengerName.toList, conductor)
-  
-}
