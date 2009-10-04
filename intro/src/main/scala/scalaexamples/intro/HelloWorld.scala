b8ef9f4d93656c86bd07f1895481b75297170e0b
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-10-04 18:05:06 +0200

    Removed solutions for the intro project

--- intro/src/main/scala/scalaexamples/intro/HelloWorld.scala
+++ intro/src/main/scala/scalaexamples/intro/HelloWorld.scala
@@ -3,15 +3,12 @@ package scalaexamples.intro
 // Print out HelloWorld
 object HelloWorld1 extends Application {
   	
-  println("Hello world1")
-	
 }
 
 // Print out HelloWorld
 object HelloWorld2 {
   
   def main(args: Array[String]) {
-    println("Hello world2")
   }
 	
 }
--- intro/src/main/scala/scalaexamples/intro/Stuff.scala
+++ intro/src/main/scala/scalaexamples/intro/Stuff.scala
@@ -3,27 +3,19 @@ package scalaexamples.intro
 class Stuff {
   
   // Override the toString method to say "Stuff"
-  override def toString = "Stuff"
   
-  // Define a constant saying "This is a constant"
-  val const = "This is a constant"
+  // Define a constant 'const' saying "This is a constant"
   
-  // Define a variable saying "This is a variable"
-  var variable = "This is a variable"
+  // Define a variable 'variable' saying "This is a variable"
   
   // Define a method saying "Hello " to whatever input
-  def sayHello(name: String) = "Hello " + name
   
   // Deine a method which prints hello, returning Unit (void)
-  def printHello(name: String) {
-    println("Hello " + name)
-  }
 
 }
 
 object Stuff {
   
   // Define a method that returns "StuffObject"
-  def getString = "StuffObject"
   
 }
