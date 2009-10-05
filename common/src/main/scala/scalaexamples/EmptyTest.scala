5e4e2366c1d6587217d5a46d00e6e5cd5b51d3a7
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-10-05 18:35:49 +0200

    Comments on EmptyTest

--- common/src/main/scala/scalaexamples/EmptyTest.scala
+++ common/src/main/scala/scalaexamples/EmptyTest.scala
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 class EmptyTest {
-  
+
+  // Write a unit test. Make it fail, and then make it pass.
   @Test def testRun(){
     println("Running empty test to make things pass")
   }
