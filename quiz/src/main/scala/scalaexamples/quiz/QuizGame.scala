aaa189ec4a74611b6fc522766036f8549e0d9133
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-10-20 19:42:31 +0200

    Parsed the xml into quiz objects.

--- quiz/src/main/scala/scalaexamples/quiz/QuizGame.scala
+++ quiz/src/main/scala/scalaexamples/quiz/QuizGame.scala
@@ -1,10 +1,31 @@
 package scalaexamples.quiz
 
+import xml._
+
 object QuizGame {
   
   def main(args: Array[String]) {
-    val parsed = xml.XML.loadFile("src/main/resources/quiz.xml")
-    println(parsed)
+    val xmlFromFile = xml.XML.loadFile("src/main/resources/quiz.xml")
+    val quiz = parseXml(xmlFromFile)
+    println(quiz)
+  }
+  
+  def parseXml(xml: Elem): Quiz = {
+    val title = (xml \ "title").text
+    val items = (xml \\ "item")
+    val questions = items.map(parseElement)
+    
+    Quiz(title, questions.toList)
+  }
+  
+  def parseElement(node: NodeSeq) = {
+    val text = (node \ "question").text
+    val answers = (node \ "option").map(option => Answer(option.text, option.attribute("correct").map(_ == "y").getOrElse(false)))
+    Question(text, answers.toList)
   }
 
 }
+
+case class Quiz(title: String, questions: Seq[Question])
+case class Question(text: String, answers: Seq[Answer])
+case class Answer(text: String, correct: Boolean)
