ca9d4c20ce8bfda403423edead80011f471495a2
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-10-20 21:28:45 +0200

    A working game!

--- quiz/src/main/scala/scalaexamples/quiz/QuizGame.scala
+++ quiz/src/main/scala/scalaexamples/quiz/QuizGame.scala
@@ -7,7 +7,40 @@ object QuizGame {
   def main(args: Array[String]) {
     val xmlFromFile = xml.XML.loadFile("src/main/resources/quiz.xml")
     val quiz = parseXml(xmlFromFile)
-    println(quiz)
+    println("Welcome to our faboulus quiz called '" + quiz.title + "'")
+    
+    var correctAnswer = 0
+    for (q <- quiz.questions) {
+      println("\n" + q.text)
+      q.answers.zipWithIndex.foreach{case (a, i) => println(i + 1 + ". " + a.text)}
+      
+      correctAnswer += { 
+        if (readAnswer(q).correct) {
+        	println("Correct")
+        	1
+        }
+        else {
+        	println("Incorrect")
+        	0
+      	}
+      }
+    }
+    println("\nYou got " + correctAnswer + " correct of " + quiz.questions.length + " possible")
+  }
+  
+  def readAnswer(q: Question) = {
+    var answer: Answer = null
+    while (answer == null) {
+    	answer = try { 
+    		println("Enter you answer:")
+    		val res = readLine.toInt - 1
+    		q.answers(res)
+    	}
+    	catch {
+    		case e => println("Incorrect answer"); null 
+    	}
+     }
+    answer
   }
   
   def parseXml(xml: Elem): Quiz = {
@@ -26,6 +59,6 @@ object QuizGame {
 
 }
 
-case class Quiz(title: String, questions: Seq[Question])
-case class Question(text: String, answers: Seq[Answer])
+case class Quiz(title: String, questions: List[Question])
+case class Question(text: String, answers: List[Answer])
 case class Answer(text: String, correct: Boolean)
