04af9e6f5bd340d21166b5e164793c0697433b38
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-10-22 12:34:26 +0200

    Added one more mock question.

--- quiz/src/main/scala/scalaexamples/quiz/MockQuizProvider.scala
+++ quiz/src/main/scala/scalaexamples/quiz/MockQuizProvider.scala
@@ -3,15 +3,22 @@ package scalaexamples.quiz
 object MockQuiz extends QuizProvider {
 
   val quiz =
-	  		Quiz("The Example Quiz", 
+             Quiz("The Example Quiz", 
                   List(Question("In which continent is the country Japan located?",
                                 List(Answer("Asia",true), 
                                      Answer("Europe",false), 
                                      Answer("Africa",false), 
                                      Answer("America",false)
-                                	)
-                  				)
-                       	)
-                 )
+                                     )
+                                ),
+                       Question("Which one cannot swim?",
+                                List(Answer("Tuna",true), 
+                                     Answer("Cow",false), 
+                                     Answer("Whale",false), 
+                                     Answer("Lobster",false)
+                                     )
+                                )
+                       )
+             )
   
 }
