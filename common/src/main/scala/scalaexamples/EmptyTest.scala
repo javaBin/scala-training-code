e06ecc4f7c9866a24216643627253e4cddb71679
Alf Kristian Stoyle <alf.kristian@gmail.com>
2009-10-30 12:16:53 +0100

    Annoying println from emptytest.

--- common/src/main/scala/scalaexamples/EmptyTest.scala
+++ common/src/main/scala/scalaexamples/EmptyTest.scala
@@ -8,8 +8,6 @@ import org.junit.runners.JUnit4
 class EmptyTest {
 
   // Write a unit test. Make it fail, and then make it pass.
-  @Test def testRun(){
-    println("Running empty test to make things pass")
-  }
+  @Test def testRun{}
 
 }
